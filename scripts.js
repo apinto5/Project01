363117e1e48464d92124492472c2c56f231b5ae5
apinto5 <apinto5@hawk.iit.edu>
2022-03-24 17:38:31 -0400

    Configure two index.html files for new Javascript; added new Javascript to utilize event handler

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-function nextpage(){
-  window.location.href="billing.html"
+'use strict';
+
+var html = document.querySelector('html');
+html.classList.add('js');
+
+if (html.id === 'shipping-info') {
+	var pageForm = document.querySelector('form[name="shippingform"]');
+	var elementList = document.querySelectorAll('#shippingform input');
+	pageForm.addEventListener('submit', submitForm);
+}
+if (html.id === 'billing-info') {
+	var pageForm = document.querySelector('form[name="billingform"]');
+	var elementList = document.querySelectorAll('#billingform input');
+	pageForm.addEventListener('submit', submitForm);
+}
+
+function submitForm(event){
+	var target = event.target;
+	event.preventDefault();
+	var isError = false;
+	for (var i = 0, element; element = elementList[i++];){
+		if (element.value === "")
+			isError = true;
+	}
+	if(isError)
+		alert("Some fields are blank. Please fill out entire form.");
+	if(!isError)
+		window.location = "../billing/index.html";
 }
